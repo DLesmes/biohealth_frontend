9d56d79200efad8e4e28b5eb70c2f82dbe71f507
DLesmes <ing.dlesmes@gmail.com>
2021-11-20 18:18:11 -0500

    config local files

--- Biohealth_backend/src/app.ts
+++ Biohealth_backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import medicosRoutes from './routes/medicos';
+import config from './config/config';
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 medicosRoutes(app);
 
 
-app.listen(port, () => {
-return console.log(`servidor corriendo sobre el puerto ${port}`)
+app.listen(config.PORT, () => {
+    return console.log(`servidor corriendo sobre el puerto ${config.PORT}`)
 });
